44b3e7aefb8e34428130f3b485f2d6c9cd9723b6
madu munachi <madustanley1@gmail.com>
2020-04-15 23:05:21 +0100

    modified project

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -4,14 +4,38 @@ class MemeGenerator extends React.Component {
     constructor() {
     super()
     this.state = {
-        
+    topText: "",
+    bottomText: '',
+    randomImg: '',
+    allMemeImgs: []
+
     }
+    }
+
+    componentDidMount() {
+        fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+        .then(response => {
+            const{memes} = response.data
+            console.log(memes[5])
+            this.setState({allMemeImgs: memes})
+        })
     }
 
     render() {
         return (
             <div>
-
+            <form>
+                <input 
+                name='topText'
+                type='text'
+                value='this.state.topText'
+                />
+                <input 
+                
+                />
+                <button>Gen</button>
+            </form>
             </div>
         )
     }
