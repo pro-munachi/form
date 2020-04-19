284e20a559816095ffc8c4d82346c524dd1aa630
madu munachi <madustanley1@gmail.com>
2020-04-19 18:19:48 +0100

    meme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
 import Header from './Header'
 import MemeGenerator from './MemeGenerator'
+import About from './Components/About'
 
 function App() {
   return (
-    <div>
-    <Header />
-    <MemeGenerator />  
-    </div>
+     <BrowserRouter>
+     <Route path='/' component={About} exact />
+     <Route path='/meme' component={Header} />
+     <Route path='/meme' component={MemeGenerator} />
+     </BrowserRouter>
+    
   )
 }
 
---
+++ src/Components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+         <figure>  
+         <img src='/asset/img/Snapchat-322511253.jpg' alt='' />   
+         <blockquote>Munachiso is generous and kind</blockquote>
+         <figcaption>Munachi - 2020</figcaption>
+         <hr />
+         </figure> 
+         <div>
+             <h2>About Madu Stanley Munachiso</h2>
+             <p>Madu Stanley Munachiso resides in Owerri Municipal Local Government Area in Imo State, Nigeria. He was introduced to programming by his elder brother in the year 2019.
+               he successfully has HTML,CSS,Javascript,React.js under his arms and is still striving to learn Angular,Node,Php, and many others.</p>
+         </div>
+        </div>
+    )
+}
+
+export default About 
